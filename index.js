93c11fd13a7211bceb17eaec4170a05cd1cf853e
danilor4 <danilolima180@gmail.com>
2025-07-13 19:09:09 -0300

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-let ranqueada
+// calculadora de partidas ranqueadas
+quantDeVitorias()
+function quantDeVitorias() {
+    let vitorias =0;
+    let derrotas =0;
+    
+    if (vitorias <=10){
+        console.log("O personagem esta no nivel Ferro");
+    }
+    else if (vitorias > 10 && vitorias<=20) {
+        console.log("O Personagem esta no nivel Bronze");
+    }
+    else if(vitorias > 20 && vitorias <= 50) {
+        console.log("O personagems esta no nivel Prata);
+    }
+
+}
